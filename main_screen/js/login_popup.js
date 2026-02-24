6a3008bf5df4cceb6bc67a9282b0cd9163a828be
LeeYongChan <220350503+defenxe@users.noreply.github.com>
2026-02-24 11:03:33 +0900

    Update login_popup.js

--- main_screen/js/login_popup.js
+++ main_screen/js/login_popup.js
@@ -2,15 +2,12 @@
 var target = document.querySelectorAll('.btn_open');
 // 닫기버튼
 var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
-var targetID;
 
 const popWrap = document.querySelector("#pop_info_1");
 
 // 팝업 열기
-// for 문을 사용해 열기 버튼 개수(target.length)만큼 실행함
 for(var i = 0; i < target.length; i++){
   target[i].addEventListener('click', function(){
-    // #pop_info_1(pop_wrap)의 style(css) 속 display를 block으로 설정해서 숨겨진 팝업창이 보이게 하기
     popWrap.style.display = 'block';
   });
 }
@@ -18,7 +15,6 @@ for(var i = 0; i < target.length; i++){
 // 팝업 닫기
 for(var j = 0; j < target.length; j++){
   btnPopClose[j].addEventListener('click', function(){
-    //this(btn_close)의 조부모인 pop_wrap의 display: block을 none으로 바꿔서 팝업창 안보이게 하기
     popWrap.style.display = 'none';
   });
 }
